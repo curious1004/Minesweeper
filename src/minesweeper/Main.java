9df36a44a9f7e5f3420809727e98f25612755b10
chien <curious1004@gmail.com>
2018-09-03 10:27:46 +0800

    New commit

---
+++ src/minesweeper/Main.java
@@ -0,0 +1,15 @@
+package minesweeper;
+
+public class Main {
+	public Main() {
+		super();
+		//chooseLevel=8;
+		//Hello2();
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
